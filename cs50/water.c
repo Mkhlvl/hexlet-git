9b697cd6b99abd17b4f719a330cda88adf18f510
helen <mikhaleva.helena@gmail.com>
2022-02-04 13:56:35 +0700

    .

---
+++ cs50/water.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main (void)
+{
+int time;
+printf("how much times did you spend at bath?\n");
+scanf("%d", &time);
+printf("you spent ");
+printf("%d ", time/10);
+printf("bottles of water");
+return 0;
+}
